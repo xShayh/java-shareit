6d3393f220b7d063c228173daf1e740f7dd1ead5
shayh <seregaadams20@yandex.ru>
2025-03-22 08:28:24 +0300

    Правки CheckStyle

--- server/src/main/java/ru/practicum/shareit/item/mapper/CommentMapper.java
+++ server/src/main/java/ru/practicum/shareit/item/mapper/CommentMapper.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.item.mapper;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.model.Comment;
 import ru.practicum.shareit.user.model.User;
